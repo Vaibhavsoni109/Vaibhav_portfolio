abff782f83d874addcd37f1baa5b7728578298f4
Vaibhav Kant Soni <94217024+Vaibhavsoni109@users.noreply.github.com>
2024-11-09 16:54:29 +0530

    about

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import './App.scss'
 import { About, Footer, Header, Skills, Testimonials, Work } from './container';
 import { Navbar } from './components';
 
+
+
 function App() {
 
 
--- frontend/src/client.js
+++ frontend/src/client.js
@@ -1,15 +1,19 @@
 import sanityClient from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
+
+
 // const id= process.env.REACT_APP_SANITY_PROJECT_ID;
 // const token= process.env.REACT_APP_SANITY_TOKEN;
 // console.log(process.env.REACT_APP_SANITY_PROJECT_ID)
+const id = "gywflrp4"; // Replace with actual ID temporarily
+const token = "sk859HcHvGkHtLTmHkLpMQK7nGZOmQIuCWVncMYddHeGTi5gRbZXE1iCtb237Caoyd9viv0vioJyn6x7zGnszlw6MuCKZNpto91xfhEAk9mAnlVbppdAoUdRBY2dm7dNBFSB9FjRqds4Edk7zzlJsvVcIO4rGUxty6BqGRIqdRIAgr0he1ZC";
 export const client= sanityClient({
-    projectId:process.env.REACT_APP_SANITY_PROJECT_ID,
+    projectId:id,
     dataset:'production',
     apiVersion:'2024-01-01',
     useCdn:true,
-    token:process.env.REACT_APP_SANITY_TOKEN ,
+    token:token,
 
 })
 const builder = imageUrlBuilder(client);
