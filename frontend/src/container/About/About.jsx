7258b5af39528d4653a933514c4b84bd76698eda
Vaibhav Kant Soni <94217024+Vaibhavsoni109@users.noreply.github.com>
2024-11-10 11:03:01 +0530

    about and Navigation and socialMedaicon

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-// import { AppWrap, MotionWrap } from '../../wrapper';
+
 import './About.scss';
 import { urlFor, client } from '../../client';
+import AppWrap from '../../wrapper/AppWrap';
 
 const About = () => {
   const [abouts, setAbouts] = useState([]);
@@ -38,9 +39,4 @@ const About = () => {
     </>
   );
 };
-export default About
-// export default AppWrap(
-//   MotionWrap(About, 'app__about'),
-//   'about',
-//   'app__whitebg',
-// );
+export default AppWrap(About,'about')
