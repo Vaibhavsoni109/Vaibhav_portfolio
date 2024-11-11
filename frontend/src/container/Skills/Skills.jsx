d60f4875d5735eb4a161bf50f2d43b23afaba1d8
Vaibhav Kant Soni <94217024+Vaibhavsoni109@users.noreply.github.com>
2024-11-11 16:47:37 +0530

    skill part done

--- frontend/src/container/Skills/Skills.jsx
+++ frontend/src/container/Skills/Skills.jsx
@@ -1,16 +1,51 @@
 import React, { useState, useEffect } from 'react';
-import ReactTolltip from 'react-tooltip'
+import { Tooltip as ReactTooltip } from 'react-tooltip';
 import { motion } from 'framer-motion';
 import { urlFor, client } from '../../client';
-
 import AppWrap from '../../wrapper/AppWrap';
-import './Skills.scss'
-const Skills = () => {
-  return (
-    <div>Skills
-      
-    </div>
-  )
-}
+import './Skills.scss';
 
-export default Skills
+const Skills = () => {
+  const [experience, setExperience] = useState([]);
+  const [skills, setSkill] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "experiences"]';
+    const skillsQuery = '*[_type == "skills"]';
+
+    client.fetch(query).then((data) => {
+      setExperience(data);
+    });
+
+    client.fetch(skillsQuery).then((data) => {
+      setSkill(data);
+    });
+  }, []); // Dependency array should be outside of `then`
+
+  return (
+    <>
+      <h2 className="head-text">Skills & Experience</h2>
+      <div className="app__skills-container">
+        <motion.div className="app__skills-list">
+          {skills.map((skill)=>(
+            <motion.div
+            whileInView={{opacity:[0, 1]}}
+            transition={{duration:0.5}}
+            className='app__skills-item app__flex'
+            key={skills.name}>
+                <div className="app__flex" style={{backgroundColor:skill.bgColor}}>
+                <img src={urlFor(skill.icon)} alt={skill.name} />
+
+                </div>
+                <p className='p-text'>
+                  {skill.name}
+                </p>
+            </motion.div>
+         ) )}
+        </motion.div>
+      </div>
+    </>
+  );
+};
+
+export default Skills;
