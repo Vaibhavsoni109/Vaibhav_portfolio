aef488b3648c9fb14566eb515ed4ec96537ec7c2
Vaibhav Kant Soni <94217024+Vaibhavsoni109@users.noreply.github.com>
2024-11-11 14:54:28 +0530

    error fix

--- frontend/src/container/Skills/Skills.jsx
+++ frontend/src/container/Skills/Skills.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import ReactTolltip from 'react-tooltip'
+import { motion } from 'framer-motion';
+import { urlFor, client } from '../../client';
+
+import AppWrap from '../../wrapper/AppWrap';
 import './Skills.scss'
 const Skills = () => {
   return (
