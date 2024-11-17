fd0fc82f7727067c7ca8b7d342e3392a12e8987b
Vaibhav Kant Soni <94217024+Vaibhavsoni109@users.noreply.github.com>
2024-11-17 14:53:08 +0530

    every section done

--- frontend/src/container/Pricing/Pricing.jsx
+++ frontend/src/container/Pricing/Pricing.jsx
@@ -1,15 +1,55 @@
 import React from 'react'
 import './Pricing.scss';
+import { FaCheck } from "react-icons/fa";
 
 const Pricing = () => {
     return (
         <>
             <div className=" main">
-                <div id="PrSize">Basic 
+                <div id="PrSize">
+                    <h2>Basic
+                        <h1>Rs. 10k</h1>
+                        <h6>/month</h6>
+                    </h2>
+                    <br />
+                    <ul>
+                        <li id='content'><FaCheck style={{ color: 'green' }} /> <span>Only Frontend Development</span>
+                        </li>
+                        <li id='content'><FaCheck style={{ color: 'green' }} /><span>Hosting not included
+                        </span>
+                        </li>
+                        <li id='content'><FaCheck style={{ color: 'green' }} /><span>Maintenance not included</span></li>
+                        <li id='content'><FaCheck style={{ color: 'green' }} /><span>Testing Included</span></li>
+                    </ul>
+                    <button className='btn'>Discuss now</button>
                 </div>
-                <div id="PrSize">Medium</div>
-                <div id="PrSize">Pro</div>
-                </div>
+                <div id="PrSize"> <h2>Medium Pack
+                        <h1>Rs. 25k</h1>
+                        <h6>/month</h6>
+                    </h2>
+                    <br />
+                    <ul>
+                        <li id='content'><FaCheck style={{ color: 'green' }} /> <span>Frontend & Backend Development</span>
+                        </li>
+                        <li id='content'><FaCheck style={{ color: 'green' }} /><span>Only Frontend Development</span>
+                        </li>
+                        <li id='content'><FaCheck style={{ color: 'green' }} /><span>Only Frontend Development</span></li>
+                        <li id='content'><FaCheck style={{ color: 'green' }} /><span>Only Frontend Development</span></li>
+                    </ul></div>
+                <div id="PrSize"> <h2>Basic
+                        <h1>Rs. 10k</h1>
+                        <h6>/month</h6>
+                    </h2>
+                    <br />
+                    <ul>
+                        <li id='content'><FaCheck style={{ color: 'green' }} /> <span>Only Frontend Development</span>
+                        </li>
+                        <li id='content'><FaCheck style={{ color: 'green' }} /><span>Only Frontend Development</span>
+                        </li>
+                        <li id='content'><FaCheck style={{ color: 'green' }} /><span>Only Frontend Development</span></li>
+                        <li id='content'><FaCheck style={{ color: 'green' }} /><span>Only Frontend Development</span></li>
+                    </ul></div>
+            </div>
 
 
         </>
