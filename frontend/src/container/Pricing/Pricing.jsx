f49c21ca40af5954aee197014d0f625dd732eadd
Vaibhav Kant Soni <94217024+Vaibhavsoni109@users.noreply.github.com>
2024-11-18 15:18:21 +0530

    Pricing

--- frontend/src/container/Pricing/Pricing.jsx
+++ frontend/src/container/Pricing/Pricing.jsx
@@ -7,7 +7,7 @@ const Pricing = () => {
         <>
             <div className=" main">
                 <div id="PrSize">
-                    <h2>Basic
+                    <h2>Basic Pack
                         <h1>Rs. 10k</h1>
                         <h6>/month</h6>
                     </h2>
@@ -21,34 +21,34 @@ const Pricing = () => {
                         <li id='content'><FaCheck style={{ color: 'green' }} /><span>Maintenance not included</span></li>
                         <li id='content'><FaCheck style={{ color: 'green' }} /><span>Testing Included</span></li>
                     </ul>
-                    <button className='btn'>Discuss now</button>
+                    <button className='btn'><h4>Discuss now</h4></button>
                 </div>
                 <div id="PrSize"> <h2>Medium Pack
-                        <h1>Rs. 25k</h1>
-                        <h6>/month</h6>
-                    </h2>
+                    <h1>Rs. 25k</h1>
+                    <h6>/month</h6>
+                </h2>
                     <br />
                     <ul>
                         <li id='content'><FaCheck style={{ color: 'green' }} /> <span>Frontend & Backend Development</span>
                         </li>
-                        <li id='content'><FaCheck style={{ color: 'green' }} /><span>Only Frontend Development</span>
+                        <li id='content'><FaCheck style={{ color: 'green' }} /><span>Basic Web Hostingt</span>
                         </li>
-                        <li id='content'><FaCheck style={{ color: 'green' }} /><span>Only Frontend Development</span></li>
-                        <li id='content'><FaCheck style={{ color: 'green' }} /><span>Only Frontend Development</span></li>
-                    </ul></div>
-                <div id="PrSize"> <h2>Basic
-                        <h1>Rs. 10k</h1>
-                        <h6>/month</h6>
-                    </h2>
+                        <li id='content'><FaCheck style={{ color: 'green' }} /><span>Free Domain not included</span></li>
+                        <li id='content'><FaCheck style={{ color: 'green' }} /><span>Limited Maintenance for 1 month</span></li>
+                    </ul><button className='btn'><h4>Discuss now</h4></button></div>
+                <div id="PrSize"> <h2>Premium Pack
+                    <h1>Rs. 10k</h1>
+                    <h6>/month</h6>
+                </h2>
                     <br />
                     <ul>
-                        <li id='content'><FaCheck style={{ color: 'green' }} /> <span>Only Frontend Development</span>
+                        <li id='content'><FaCheck style={{ color: 'green' }} /> <span>Fullstack Development</span>
                         </li>
-                        <li id='content'><FaCheck style={{ color: 'green' }} /><span>Only Frontend Development</span>
+                        <li id='content'><FaCheck style={{ color: 'green' }} /><span>Premium Web Hosting</span>
                         </li>
-                        <li id='content'><FaCheck style={{ color: 'green' }} /><span>Only Frontend Development</span></li>
-                        <li id='content'><FaCheck style={{ color: 'green' }} /><span>Only Frontend Development</span></li>
-                    </ul></div>
+                        <li id='content'><FaCheck style={{ color: 'green' }} /><span>Free Domaint</span></li>
+                        <li id='content'><FaCheck style={{ color: 'green' }} /><span>Free Maintenance for 1st year</span></li>
+                    </ul><button className='btn'><h4>Discuss now</h4></button></div>
             </div>
 
 
