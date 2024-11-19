21b7853c57fcc3ee1a127a314e4d269e4f5f5804
Vaibhav Kant Soni <94217024+Vaibhavsoni109@users.noreply.github.com>
2024-11-19 18:02:40 +0530

    Social media

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
 import {BsTwitter, BsInstagram} from "react-icons/bs"
-import { FaFacebook } from 'react-icons/fa'
+import { FaFacebook, FaGithub, FaInstagram, FaLinkedinIn, FaWhatsapp } from 'react-icons/fa'
 const SocialMedia = () => {
   return (
     <div className='app__social'>
         <div>
-            <BsTwitter/>
+        <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+        <FaGithub/>
+        </a>
         </div>
       
         <div>
-            <BsInstagram/>
+        <a href="https://www.linkedin.com/in/vaibhav-soni-822b64242/" target="_blank" rel="noopener noreferrer">
+        <FaLinkedinIn/>
+        </a>
         </div>
       
         <div>
-            <FaFacebook/>
+        <a href="https://www.instagram.com/__vaibhav__soni_/" target="_blank" rel="noopener noreferrer">
+        <FaInstagram/>
+        </a>
+        </div>
+        <div>
+        <a href="https://wa.me/7706004827" target="_blank" rel="noopener noreferrer">
+        <FaWhatsapp/>
+        </a>
         </div>
       
     </div>
